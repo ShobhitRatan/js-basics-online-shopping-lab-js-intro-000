d82626724849480aec64525aeadc270d956e38a6
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-17 23:30:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,22 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.';
+  }
+  else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+  }
+  else if (cart.length === 2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+  }
+  else {
+    var moreThanTwoItems = 'In your cart, you have ';
+    for (var i = 0; i < cart.length - 1; i++) {
+      moreThanTwoItems += `${cart[i].itemName} at $${cart[i].itemPrice},`;
+    }
+    return moreThanTwoItems + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`;
+  }
 }
 
 function total() {
